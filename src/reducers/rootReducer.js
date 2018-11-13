353cd4f1e54e78d09483357cf99be4b84656c24a
V S Bharath <bharathcse5t6@gmail.com>
2018-11-13 11:28:11 +0530

    mapstatetoprops

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-  posts: []
+  posts: [
+    { id: "1", title: "helix", body: "lorem ipsum" },
+    { id: "2", title: "charmendar", body: "lorem ipsum" },
+    { id: "3", title: "drogo", body: "lorem ipsum" }
+  ]
 };
 
 const rootReducer = (state = initState, action) => {
